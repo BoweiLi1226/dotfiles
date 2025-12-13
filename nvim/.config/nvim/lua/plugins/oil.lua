e246e34b313a0979b531bf630250b248277d44de
bowei <boweili1995@gmail.com>
2025-12-12 20:45:05 -0500

    Configure kotlin_lsp and add shortcut for oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -9,4 +9,9 @@ return {
         },
         use_default_keymaps = true,
     },
+    config = function(_, opts)
+        local oil = require 'oil'
+        oil.setup(opts)
+        vim.keymap.set('n', '<leader>e', function() vim.cmd('Oil') end, { desc = 'Open parent directory' })
+    end
 }

13c4b946a97b32c9fbe2c740ed19eb81867504e8
bowei <boweili1995@gmail.com>
2026-01-10 10:51:08 -0500

    Add onedark theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,27 +1,33 @@
 return {
-    {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        priority = 1000,
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			auto_integrations = true,
+		},
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"vague-theme/vague.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000,
         opts = {
-            transparent_background = true,
-            auto_integrations = true,
-        },
-    },
-    {
-        'rose-pine/neovim',
-        name = 'rose-pine',
-    },
-    {
-        'folke/tokyonight.nvim',
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-    {
-        'vague-theme/vague.nvim',
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
+            style = 'darker',
+        }
+	},
 }

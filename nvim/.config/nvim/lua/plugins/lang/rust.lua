1b502f9240560fdc8e31b70411d19823f2202909
bowei <boweili1995@gmail.com>
2026-01-27 20:53:26 -0500

    Add formatter configuration for rust

--- nvim/.config/nvim/lua/plugins/lang/rust.lua
+++ nvim/.config/nvim/lua/plugins/lang/rust.lua
@@ -1,24 +1,36 @@
 return {
-    {
-        'nvim-treesitter/nvim-treesitter',
-        branch = 'main',
-        optional = true,
-        opts = {
-            ensure_installed = { 'rust' },
-        },
-        opts_extend = { 'ensure_installed' },
-    },
-    {
-        'mason-org/mason.nvim',
-        optional = true,
-        opts = {
-            ensure_installed = { 'rust-analyzer' },
-        },
-        opts_extend = { 'ensure_installed' },
-    },
-    {
-        'mrcjkb/rustaceanvim',
-        version = '^6',
-        lazy = false,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		optional = true,
+		opts = {
+			ensure_installed = { "rust" },
+		},
+		opts_extend = { "ensure_installed" },
+	},
+	{
+		"mason-org/mason.nvim",
+		optional = true,
+		opts = {
+			ensure_installed = {
+				-- "rust-analyzer",
+				-- "rustfmt",
+			},
+		},
+		opts_extend = { "ensure_installed" },
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^7",
+		lazy = false,
+	},
+	{
+		"stevearc/conform.nvim",
+		optional = true,
+		opts = {
+			formatters_by_ft = {
+				rust = { "rustfmt" },
+			},
+		},
+	},
 }

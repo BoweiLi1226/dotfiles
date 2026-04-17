12d8c51b0d0fb2f53b5787bc756fc83ebe66131f
Bowei Li <cesterty024@gmail.com>
2026-04-16 21:01:10 -0400

    Remove unused plugins

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -23,17 +23,4 @@ return {
 		priority = 1000,
 		opts = {},
 	},
-	{
-		"vague-theme/vague.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
-	{
-		"navarasu/onedark.nvim",
-		priority = 1000,
-		opts = {
-			style = "darker",
-		},
-	},
 }
--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -14,27 +14,6 @@ return {
 		},
 	},
 	{
-		"akinsho/bufferline.nvim",
-		enabled = false,
-		version = "*",
-		event = "VeryLazy",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		config = function()
-			local bufferline = require("bufferline")
-			bufferline.setup({
-				options = {
-					offsets = {
-						{
-							filetype = "snacks_layout_box",
-							text = "File Explorer",
-							text_align = "center",
-						},
-					},
-				},
-			})
-		end,
-	},
-	{
 		"folke/noice.nvim",
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module='...'` entries

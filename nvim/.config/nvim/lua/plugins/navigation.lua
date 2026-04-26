f390ea26630071027650c6f419a36a2e6226eb87
Bowei Li <boweili1995@gmail.com>
2026-04-26 10:04:36 -0400

    Remove unused customization for flash nvim

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -3,16 +3,7 @@ return {
 		"folke/flash.nvim",
 		event = "VeryLazy",
 		---@type Flash.Config
-		opts = {
-			modes = {
-				search = {
-					enabled = true,
-				},
-				char = {
-					jump_labels = true,
-				},
-			},
-		},
+		opts = {},
 		keys = {
 			{
 				"s",

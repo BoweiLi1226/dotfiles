9bc977e47c9d9642f3df1050d601d4dd2220c078
Bowei Li <cesterty024@gmail.com>
2026-09-08 19:54:58 -0400

    Change color scheme to kanagawa dragon

--- nvim/.config/nvim/lua/config/color-scheme.lua
+++ nvim/.config/nvim/lua/config/color-scheme.lua
@@ -1,3 +1,3 @@
-vim.cmd.colorscheme("catppuccin-mocha")
+vim.cmd.colorscheme("kanagawa-dragon")
 
 vim.opt.termguicolors = true
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,14 @@ return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
+		lazy = false,
 		priority = 1000,
+		opts = {},
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
 	},
 }

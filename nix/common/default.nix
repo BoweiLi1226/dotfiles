4bd46ad88d79f433ee44929610eb117af1471a8e
Bowei Li <boweili1226@gmail.com>
2026-09-25 22:10:49 -0400

    Remove unused fields

--- nix/common/default.nix
+++ nix/common/default.nix
@@ -9,10 +9,6 @@
   # Set the Git commit hash for this system configuration.
   system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
 
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = [];
-
   fonts.packages = with pkgs; [
     nerd-fonts.fantasque-sans-mono
   ];
